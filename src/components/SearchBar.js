ce7e70dad7cdbc8861bd6b9bb68c5e1b84566a1b
Nick Wright <nwright@egnyte.com>
2023-03-26 08:52:46 -0400

    route and links

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function Searchbar(props) {
+    let [searchTerm, setSearchTerm] = useState('')
+
+    return (
+        <form onSubmit={e => props.handleSearch(e, searchTerm)} >
+            <input type="text" placeholder="Enter a search term here" onChange={e => setSearchTerm(e.target.value)}/>
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default Searchbar
